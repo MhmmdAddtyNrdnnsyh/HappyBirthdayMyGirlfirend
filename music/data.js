e46b15c81f0f18cca83686b7ee523f516d91dceb
MhmmdAddtyNrdnnsyh <cuplyssss@gmail.com>
2025-05-25 21:02:26 +0700

    selesai sudah

---
+++ music/data.js
@@ -0,0 +1,14 @@
+let songs = [
+  {
+    name: "Sampai Jadi Debu",
+    path: "music/src/SampaiJadiDebu.mp3",
+    artis: "Banda Neira",
+    cover: "music/cover/SampaiJadiDebu.jpg",
+  },
+  {
+    name: "Untuk Mencintaimu",
+    path: "music/src/Untuk Mencintaimu.mp3",
+    artis: "Seventeen",
+    cover: "music/cover/unnamed.jpg",
+  }
+];
