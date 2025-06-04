6e932f8e9d508c66640dbeaeaa9aa5481c112ce5
MhmmdAddtyNrdnnsyh <cuplyssss@gmail.com>
2025-06-05 00:11:56 +0700

    update lagu oyy

--- music/data.js
+++ music/data.js
@@ -10,5 +10,23 @@ let songs = [
     path: "music/src/Untuk Mencintaimu.mp3",
     artis: "Seventeen",
     cover: "music/cover/unnamed.jpg",
-  }
+  },
+  {
+    name: "Amin Paling Serius",
+    path: "music/src/Amin Paling Serius.mp3",
+    artis: "Sal Priadi & Nadin Amizah",
+    cover: "music/cover/AminPalingSerius.jpg",
+  },
+  {
+    name: "Bunga Abadi",
+    path: "music/src/Bunga Abadi.mp3",
+    artis: "Rio Clappy",
+    cover: "music/cover/BungaAbadi.jpg",
+  },
+  {
+    name: "Monolog",
+    path: "music/src/Monolog.mp3",
+    artis: "Pamungkas",
+    cover: "music/cover/Monolog.jpg",
+  },
 ];
